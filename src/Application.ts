f4338bcf277b0405d934adaba41f341e1bbfc9d9
Ryan Dowling <ryan@ryandowling.me>
2020-09-22 00:36:37 +1000

    feat: add in Shopping Express

--- src/Application.ts
+++ src/Application.ts
@@ -14,6 +14,7 @@ import MWave from './sites/mwave';
 import CPLOnline from './sites/cplonline';
 import GameDude from './sites/gamedude';
 import AussieAppliances from './sites/aussieappliances';
+import ShoppingExpress from './sites/shoppingexpress';
 
 class Application {
     scanners: Scannable[] = [
@@ -25,6 +26,7 @@ class Application {
         new CPLOnline(),
         new GameDude(),
         new AussieAppliances(),
+        new ShoppingExpress(),
     ];
 
     async scanSites() {
---
+++ src/sites/shoppingexpress.ts
@@ -0,0 +1,53 @@
+import currency from 'currency.js';
+import type { Browser } from 'puppeteer';
+
+import type { Card, Scannable } from '../core';
+
+class ShoppingExpress implements Scannable {
+    url = 'https://www.shoppingexpress.com.au/nvidia-geforce?rf=&viewby=list';
+
+    async scan(browser: Browser) {
+        const page = await browser.newPage();
+        await page.goto(this.url);
+
+        const cards: Card[] = [];
+
+        await page.waitForSelector('.wrapper-row-thumbnail');
+        const items = await page.$$('.wrapper-row-thumbnail .panel-body');
+
+        for await (const item of items) {
+            const productNameElement = await item.$('.thumblist-desc a');
+            const productName = await page.evaluate((el) => el.textContent.trim(), productNameElement);
+            const url: string = await page.evaluate((el) => el.href, productNameElement);
+
+            if (!productName.includes('3080')) {
+                continue;
+            }
+
+            const productModelNumber = url.substr(url.lastIndexOf('/') + 1);
+
+            const productPriceElement = await item.$('.price');
+            const productPriceString = await page.evaluate((el) => el.textContent.trim(), productPriceElement);
+            const productPrice = currency(productPriceString).value;
+
+            const availabilityElement = await item.$('.availability_description');
+            const availability = await page.evaluate((el) => el.textContent.trim(), availabilityElement);
+
+            cards.push({
+                name: productName,
+                productNumber: productModelNumber,
+                price: productPrice,
+                availability,
+                url,
+            });
+        }
+
+        await page.close();
+
+        return {
+            cards,
+        };
+    }
+}
+
+export default ShoppingExpress;
