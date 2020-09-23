71c7dfe0af5f016fe6b018e66df2fde1fd34e372
Ryan Dowling <ryan@ryandowling.me>
2020-09-23 15:55:19 +1000

    fix: logging not writing to console in production

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,14 +3,10 @@ import * as path from 'path';
 import * as winston from 'winston';
 import type Transport from 'winston-transport';
 
-import { isProductionEnvironment } from './env';
-
-const isProduction = isProductionEnvironment();
-
 const logger = winston.createLogger({
     transports: [
-        !isProduction && new winston.transports.Console(),
-        isProduction && new winston.transports.File({ filename: path.resolve(__dirname, '../logs/server.log') }),
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: path.resolve(__dirname, '../../logs/server.log') }),
     ].filter(Boolean) as Transport[],
     level: config.get<string>('logging_level'),
 });
