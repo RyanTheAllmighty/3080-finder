56f6781def583994abd39e7f52184234b03486f1
Ryan Dowling <ryan@ryandowling.me>
2020-11-11 18:33:09 +1100

    fix: budgetpc scanning

--- src/sites/budgetpc.ts
+++ src/sites/budgetpc.ts
@@ -5,7 +5,7 @@ import type { Card, Scannable } from '../core';
 import { getNewPage } from '../utils/browser';
 
 class BudgetPC implements Scannable {
-    url = 'https://www.budgetpc.com.au/computer-hardware/computer-components/video-cards/geforce-rtx-3080.html';
+    url = 'https://www.bpctech.com.au/catalogsearch/result/index/?cat=21&q=3080&product_list_limit=36';
 
     async scan(browser: Browser) {
         const page = await getNewPage(browser);
@@ -13,21 +13,25 @@ class BudgetPC implements Scannable {
 
         const cards: Card[] = [];
 
-        await page.waitForSelector('.category-products');
-        const items = await page.$$('.category-products .product-listing');
+        await page.waitForSelector('.category-product-list .product-item');
+        const items = await page.$$('.category-product-list .product-item');
 
         for await (const item of items) {
-            const productNameElement = await item.$('.product-name > a');
+            const productNameElement = await item.$('.product-item-link');
             const productName = await page.evaluate((el) => el.textContent.trim(), productNameElement);
             const url: string = await page.evaluate((el) => el.href, productNameElement);
 
-            const productModelNumber = url.substr(url.lastIndexOf('/') + 1) || productName;
+            const productModelNumberElement = await item.$('.proudctListProductSku');
+            const productModelNumber = await page.evaluate(
+                (el) => el.textContent.replace('SKU:', '').trim(),
+                productModelNumberElement,
+            );
 
             const productPriceElement = await item.$('.price');
             const productPriceString = await page.evaluate((el) => el.textContent.trim(), productPriceElement);
             const productPrice = currency(productPriceString).value;
 
-            const availabilityElement = await item.$('.stockimg');
+            const availabilityElement = await item.$('.stock');
             const availability = await page.evaluate((el) => el.textContent.trim(), availabilityElement);
 
             cards.push({
